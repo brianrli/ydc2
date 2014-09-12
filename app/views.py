5b0a47e2792d59f14152c51480af367563fcd979
Brian R. Li <brian.rusong.li@gmail.com>
2014-09-12 11:53:56 -0400

    added basic bootstrap, base template

--- app/views.py
+++ app/views.py
@@ -2,20 +2,21 @@ from flask import Flask, request, render_template
 from app import app
 from config import si
 from models import image
-import urllib2
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello, World!"
+	return render_template('index.html')
 
 #search page
 @app.route('/search',methods=['GET'])
 def search():
 	#get GET request
 	request_args = request.args.to_dict()
-	
-	#forward them on to apache solr server
-	Images = si.query(**request_args).execute(constructor=image)
+
+	if(request_args):
+		Images = si.query(**request_args).execute(constructor=image)
+	else:
+		Images = None
 
 	return render_template('search.html',Images=Images)
