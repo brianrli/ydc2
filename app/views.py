c1d8d923653e51bebfd461582ba90493220fefd6
Brian R. Li <brian.rusong.li@gmail.com>
2014-09-25 14:56:47 -0400

    adjusted aesthetics

--- app/views.py
+++ app/views.py
@@ -12,23 +12,4 @@ def index():
 		Images = si.query(**request_args).execute(constructor=image)
 	else:
 		Images = si.query(publishDate="1801").execute(constructor=image)
-	return render_template('search.html',Images=Images)
-
-# # search page
-# @app.route('/search',methods=['GET','POST'])
-# def search():
-# 	# request.form['title']
-# 	request_args = request.args.to_dict()
-# 	if(request_args):
-# 		Images = si.query(**request_args).execute(constructor=image)
-# 	else:
-# 		Images = si.query(publishDate="1801").execute(constructor=image)
-# 	return render_template('search.html',Images=Images)
-
-# @app.route('/add', methods=['GET'])
-# def add(): 
-# 	print("add");
-# 	if request.method == 'GET':
-# 		d = {"publishDate",request.form['year']} 
-# 		Images = si.query(**d).execute(constructor=image)
-# 	return render_template('search.html',Images=Images)
+	return render_template('index.html',Images=Images)
